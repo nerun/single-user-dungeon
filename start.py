8709c5b04df3dc551472c568edb380d0c8ae75aa
nerun <nerunblogs@gmail.com>
2021-02-09 23:41:57 -0300

    Moving functions.

--- start.py
+++ start.py
@@ -1,8 +1,49 @@
 # -*- coding: cp1252 -*-
 # This file define what exists in the world.
-import os
+import glob, copy
 from libraries import *
 from engine import *
+
+# CREATE A DICTIONARY READING FILES IN A FOLDER
+# FilesToDict(*Path, ValidExt, If is room then put yes if not let blank)
+def FilesToDict(Path, Ext, IsRoom='no'):
+    ListOfFiles = glob.glob(Path + '*' + Ext)
+    ListOfFilesB = copy.copy(ListOfFiles)
+
+    if IsRoom.lower() == "yes":
+        for n, i in enumerate(ListOfFilesB):
+            a = i.replace(Path,'')
+            b = a.replace(Ext,'')
+            ListOfFilesB[n] = b
+
+        for n, i in enumerate(ListOfFilesB):
+            try:
+                int(i)
+            except ValueError:
+                ListOfFiles.pop(n)
+
+    Dict = {}
+
+    for i in ListOfFiles:
+        fileread = open(i,"r")
+        thisfile = fileread.readlines()
+        fileread.close()
+        for i in thisfile:
+            thisfile[0] = thisfile[0].replace('\n','')
+            if IsRoom.lower() == "yes":
+                thisfile[1] = thisfile[1].replace('\n','')
+                thisfile[2] = thisfile[2].replace('\n','')
+        thisfiletuple = tuple(thisfile)
+        Dict[thisfiletuple[0]] = thisfiletuple[1:]
+
+    return Dict
+
+# SHOW ROOM DESCRIPTION TO PLAYER IN FRIENDLY FORMAT
+# ShowRoom(FilesToDict(RoomsPath, ValidExt, 'yes'),'1')
+# Rooms = rooms dictionary
+# Number = specific room number (ID)
+def ShowRoom(Rooms, Number):
+    return prcolor(6, Rooms[Number][1]) + '\n[ Exits: ' + prcolor(7,' '.join(list(Rooms[Number][0]))) + ' ]\n' + ' '.join(Rooms[Number][2:])
 
 ClearScreen()
 
